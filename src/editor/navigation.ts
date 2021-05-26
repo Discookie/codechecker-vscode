fd5655ac12118cf6104295b6f025adcf47cf1b1c
Discookie <h8efn1@inf.elte.hu>
2021-05-26 02:59:12 +0200

    Add hotkeys and commands

--- src/editor/navigation.ts
+++ src/editor/navigation.ts
@@ -1,4 +1,4 @@
-import { commands, ExtensionContext, Range, Uri, window } from "vscode";
+import { commands, ExtensionContext, Position, Range, Uri, window } from "vscode";
 import { ExtensionApi } from "../backend/api";
 import { AnalysisPathEvent, AnalysisPathKind, DiagnosticEntry } from "../backend/types";
 import { SidebarContainer } from "../sidebar";
@@ -8,6 +8,8 @@ export class NavigationHandler {
         ctx.subscriptions.push(commands.registerCommand('codechecker.editor.toggleSteps', this.toggleSteps, this));
         ctx.subscriptions.push(commands.registerCommand('codechecker.editor.jumpToBug', this.jumpToBug, this));
         ctx.subscriptions.push(commands.registerCommand('codechecker.editor.jumpToStep', this.jumpToStep, this));
+        ctx.subscriptions.push(commands.registerCommand('codechecker.editor.nextStep', this.nextStep, this));
+        ctx.subscriptions.push(commands.registerCommand('codechecker.editor.previousStep', this.previousStep, this));
     }
 
     toggleSteps(file: Uri | string, bugIndex: number, targetState?: boolean): void {
@@ -83,6 +85,111 @@ export class NavigationHandler {
             window.showWarningMessage('Unable to find specified bug, opened its file instead');
         } else if (step === undefined) {
             window.showWarningMessage('Unable to find specified reproduction step, opened its bug instead');
+        }
+    }
+
+    getStepIndexUnderCursor(which: 'first' | 'last'): number | undefined {
+        if (window.activeTextEditor === undefined || ExtensionApi.diagnostics.activeReprPath === undefined) {
+            return undefined;
+        }
+
+        const cursor = window.activeTextEditor.selection.anchor;
+        
+        const entry = ExtensionApi.diagnostics.activeReprPath;
+        const reprPath = entry.path
+            .filter(e => e.kind === AnalysisPathKind.Event) as AnalysisPathEvent[];
+
+        let lastIdx = undefined;
+        
+        for (const [idx, path] of reprPath.entries()) {
+            // Check location first
+            if (window.activeTextEditor.document.uri.fsPath !== entry.files[path.location.file]) {
+                continue;
+            }
+
+            if (cursor.isEqual(new Position(path.location.line-1, path.location.col-1))) {
+                if (which === 'first') {
+                    return idx;
+                } else {
+                    lastIdx = idx;
+                    continue;
+                }
+            }
+
+            // Check inside the ranges
+            if (path.ranges === undefined) {
+                continue;
+            }
+
+            for (const [start, end] of path.ranges) {
+                const range = new Range(
+                    start.line-1,
+                    start.col-1,
+                    end.line-1,
+                    end.col,
+                );
+
+                if (range.contains(cursor)) {
+                    if (which === 'first') {
+                        return idx;
+                    } else {
+                        lastIdx = idx;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return undefined;
+    }
+
+    nextStep() {
+        const stepIdx = this.getStepIndexUnderCursor('last');
+
+        if (stepIdx === undefined || ExtensionApi.diagnostics.activeReprPath === undefined) {
+            return;
+        }
+        
+        const entry = ExtensionApi.diagnostics.activeReprPath;
+        const reprPath = entry.path
+            .filter(e => e.kind === AnalysisPathKind.Event) as AnalysisPathEvent[];
+
+        if (stepIdx < reprPath.length - 1) {
+            const stepLocation = reprPath[stepIdx + 1].location;
+
+            window.showTextDocument(Uri.file(entry.files[stepLocation.file]), {
+                selection: new Range(
+                    stepLocation.line - 1,
+                    stepLocation.col - 1,
+                    stepLocation.line - 1,
+                    stepLocation.col - 1
+                )
+            });
+        }
+    }
+
+    previousStep() {
+        const stepIdx = this.getStepIndexUnderCursor('first');
+
+        if (stepIdx === undefined || ExtensionApi.diagnostics.activeReprPath === undefined) {
+            return;
+        }
+        
+        const entry = ExtensionApi.diagnostics.activeReprPath;
+        const reprPath = entry.path
+            .filter(e => e.kind === AnalysisPathKind.Event) as AnalysisPathEvent[];
+
+        if (stepIdx > 0) {
+            const stepLocation = reprPath[stepIdx - 1].location;
+
+            window.showTextDocument(Uri.file(entry.files[stepLocation.file]), {
+                selection: new Range(
+                    stepLocation.line - 1,
+                    stepLocation.col - 1,
+                    stepLocation.line - 1,
+                    stepLocation.col - 1
+                )
+            });
         }
     }
 }
